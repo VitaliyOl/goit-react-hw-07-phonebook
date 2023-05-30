b780c5bbea4ba5903176b19571d264f6477b5467
VitaliyOl <oliferchuk@gmail.com>
2023-05-30 19:42:42 +0300

    upd

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import { Form, FormLabel, FormInput, FormButton } from './ContactsForm.styled';
+import {
+  Container,
+  FormLabel,
+  FormInput,
+  FormButton,
+} from './ContactsForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { createContact } from 'redux/contactSlice';
 import { getContact } from 'redux/selectors';
 
 export const ContactsForm = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
   const contacts = useSelector(getContact);
 
   const dispatch = useDispatch();
@@ -18,52 +19,46 @@ export const ContactsForm = () => {
       : dispatch(createContact({ name, number }));
   };
 
-  const handleChange = e => {
-    const { name, value } = e.currentTarget;
-    if (name === 'name') {
-      setName(value);
-    } else if (name === 'number') {
-      setNumber(value);
-    }
-  };
-
   const handleSubmit = e => {
     e.preventDefault();
 
+    const { elements } = e.target;
+
+    const name = elements.name.value;
+    const number = elements.number.value;
+
     handleAddContact({ name, number });
 
-    setName('');
-    setNumber('');
+    elements.name.value = '';
+    elements.number.value = '';
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <FormLabel>
-        Name
-        <FormInput
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={handleChange}
-          value={name}
-        />
-      </FormLabel>
-      <FormLabel>
-        Number
-        <FormInput
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChange}
-          value={number}
-        />
-      </FormLabel>
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <FormLabel>
+          Name
+          <FormInput
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </FormLabel>
+        <FormLabel>
+          Number
+          <FormInput
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </FormLabel>
 
-      <FormButton type="submit">Add contact</FormButton>
-    </Form>
+        <FormButton type="submit">Add contact</FormButton>
+      </form>
+    </Container>
   );
 };
--- src/components/ContactsForm/ContactsForm.styled.jsx
+++ src/components/ContactsForm/ContactsForm.styled.jsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-export const Form = styled.form`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   border: 1px solid black;
